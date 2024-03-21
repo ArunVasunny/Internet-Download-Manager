699b2758308b55304e12c3f77f406cc568d77ea3
ArunVasunny <arunvasunny153@gmail.com>
2024-03-21 19:58:40 +0530

    implemented downloading logic

--- IDM/src/DownloadThread.java
+++ IDM/src/DownloadThread.java
@@ -1,3 +1,34 @@
-public class DownloadThread {
-    
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class DownloadThread extends Thread{
+
+    private FileInfo file;
+    dmController manager;
+
+    public DownloadThread(FileInfo file, dmController manager)
+    {
+        this.file = file;
+        this.manager = manager;
+    }
+
+    @Override
+    public void run()
+    {
+        this.file.setStatus("DOWNLOADING");
+        this.manager.updateUI(file);
+
+        //Download logic
+        try {
+            Files.copy(new URL(this.file.getUrl()).openStream(), Paths.get(this.file.getPath()));
+            this.file.setStatus("COMPLETED");
+        } catch (Exception e) {
+            this.file.setStatus("FAILED");
+            e.printStackTrace();
+        }
+
+        this.manager.updateUI(file);
+
+    }   
 }
