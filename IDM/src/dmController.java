6dce35d8334176557b13e0188fd25313bbe668f1
ArunVasunny <arunvasunny153@gmail.com>
2024-03-22 23:27:26 +0530

    added tableview and its properties to display file info

--- IDM/src/dmController.java
+++ IDM/src/dmController.java
@@ -5,14 +5,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class dmController implements Initializable{
     
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
 
-    }
+
+    @FXML
+    private TableView<FileInfo> tableView;
 
     @FXML
     private Button downloadButton;
@@ -21,19 +23,50 @@ public class dmController implements Initializable{
     private TextField urlTextField;
 
     public int index = 0;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        TableColumn<FileInfo,String> srno = (TableColumn<FileInfo,String>) this.tableView.getColumns().get(0);
+        srno.setCellValueFactory(p -> {
+            return p.getValue().indexProperty();
+        });
+
+        TableColumn<FileInfo,String> filename = (TableColumn<FileInfo,String>)this.tableView.getColumns().get(1);
+        filename.setCellValueFactory(p -> {
+            return p.getValue().nameProperty();
+        });
+
+        TableColumn<FileInfo,String> fileurl = (TableColumn<FileInfo,String>)this.tableView.getColumns().get(2);
+        fileurl.setCellValueFactory(p -> {
+            return p.getValue().urlProperty();
+        });
+
+        TableColumn<FileInfo,String> status = (TableColumn<FileInfo,String>)this.tableView.getColumns().get(3);
+        status.setCellValueFactory(p -> {
+            return p.getValue().statusProperty();
+        });
+
+        TableColumn<FileInfo, String> action = (TableColumn<FileInfo, String>)this.tableView.getColumns().get(4);
+        action.setCellValueFactory(p->{
+            return p.getValue().actionProperty();
+        });
+
+    }
     
     @FXML
     void downloadButtonClicked(ActionEvent event) {
-        
+
         String url = urlTextField.getText().trim();
         String filename = url.substring(url.lastIndexOf("/")+1);
         // System.out.println(filename);
         String status = "STARTING";
         String action = "OPEN";
         String path = location.DOWNLOAD_PATH + File.separator+filename;
-        FileInfo file = new FileInfo((index+1), filename, url, status, action, path);
+        FileInfo file = new FileInfo((index+1) +" ", filename, url, status, action, path);
         DownloadThread thread = new DownloadThread(file, this);
         thread.run();
+        this.tableView.getItems().setAll(file);
         System.out.println("File Downloaded Successfull6y");
         
     }
