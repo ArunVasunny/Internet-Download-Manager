007df2c3d621db6b5af413ea7eb9f6f86a3403c0
ArunVasunny <arunvasunny153@gmail.com>
2024-03-28 13:08:03 +0530

    changed path location

--- IDM/src/Main.java
+++ IDM/src/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         //Fxml loading
-        Parent root = FXMLLoader.load(getClass().getResource("dm.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/FXMLS/dm.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Download Manager");
         stage.setScene(scene);
