0ee4423a0bf7ddc72365c1da75d2b2afba7369cf
ArunVasunny <arunvasunny153@gmail.com>
2024-03-31 18:06:44 +0530

    added icon to dm

--- IDM/src/Main.java
+++ IDM/src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -15,6 +16,7 @@ public class Main extends Application{
         Parent root = FXMLLoader.load(getClass().getResource("/FXMLS/dm.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/CSS/styles.css").toExternalForm());
+        stage.getIcons().add(new Image("/Images/dmIcon.png"));
 
         stage.setTitle("Download Manager");
         stage.setScene(scene);
